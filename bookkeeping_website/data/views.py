fb6e0a2588fec05e8f4c91d6cfc5bf757396c70d
Duan2baka <duan2baka@gmail.com>
2021-07-08 10:19:32 +0800

    ck4 grid created

--- bookkeeping_website/data/views.py
+++ bookkeeping_website/data/views.py
@@ -4,6 +4,7 @@ from django.core import serializers
 from iot.models import Event
 from iot.models import Venue_Event
 from .forms import NameForm
+import json
 # Create your views here.
 
 def index(request):
@@ -12,7 +13,25 @@ def index(request):
 def venue_temp_data(request):
     objs = Venue_Event.objects.all()
     data = serializers.serialize('json', objs)
+    #print(data)
+    #print('---------------------------------------------')
+    tmp=json.loads(data)
+    for i in tmp:
+        tmp[i['pk']-1]['fields']['start']=i['fields']['Date']+' '+i['fields']['Time_st']
+        tmp[i['pk']-1]['fields']['end']=i['fields']['Date']+' '+i['fields']['Time_ed']
+        tmp[i['pk']-1]['fields']['id']=i['pk']
+        #print(tmp[i['pk']-1]['fields']['start'])
+    data=json.dumps(tmp)
+    #print(data)
     return JsonResponse(data, safe=False)
 
 def result(request):
+    tmp=dict(request.POST)
+    #print(dict(request.POST))
+    ret={}
+    ret['start date']=tmp['start_date']
+    ret['start time']=tmp['start_time']
+    ret['end date']=tmp['end_date']
+    ret['end time']=tmp['end_time']
+    ret['venue']=tmp['venue list']
     return render(request, 'data/result.html')
